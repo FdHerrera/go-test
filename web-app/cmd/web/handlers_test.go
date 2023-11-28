e28388f1412dd4ecb5ad49434ea3470f36151571
Federico Herrera <federico.herrera_dev@outlook.com>
2023-11-28 11:59:45 -0300

    Extracts templates dir to a var, add test.

---
+++ web-app/cmd/web/handlers_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_application_handlers(t *testing.T) {
+	var tests = []struct {
+		name               string
+		endpoint           string
+		expectedStatusCode int
+	}{
+		{"home", "/", http.StatusOK},
+		{"404", "/fish", http.StatusNotFound},
+	}
+
+	var app application
+	routes := app.routes()
+
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	pathToTemplates = "./../../templates/"
+
+	for _, e := range tests {
+		resp, err := ts.Client().Get(ts.URL + e.endpoint)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if resp.StatusCode != e.expectedStatusCode {
+			t.Errorf("For: [%s] - Expected [%d], bug got: [%d]", e.name, e.expectedStatusCode, resp.StatusCode)
+		}
+	}
+}
